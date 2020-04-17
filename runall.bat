1893b06155789c83130593de614e772223aabda8
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-04-17 09:22:33 -0700

    move code to src

--- runall.bat
+++ runall.bat
@@ -1,7 +1,7 @@
 set J=C:\Programs\CWMS-v3.2.1.256\common\exe\runhere.exe
 cd  %~dp0
-
+copy data\sample.dss  c:\temp\
 @echo off
-for /r %%v in (*.py) do %J% jython "%%v" 
+for /r %%v in (src\*.py) do %J% jython src\"%%v" 
 
 pause
--- src/AddTenHecMath.py
+++ src/AddTenHecMath.py
@@ -4,17 +4,17 @@
 # displayinselector=true
 #  Add 10 to each value using HecMath
 #
-from hec.script import Plot
+from hec.script import Plot,MessageBox
 from hec.io import TimeSeriesContainer
 #from hec.io import PairedDataContainer"
 #from hec.io import TimeSeriesMath\n"
 #from hec.io import PairedDataMath\n"
-from hec.heclib.dss import HecDss
-
+from hec.heclib.dss import HecDss,DSSPathname
+import java
 
 #  Open the file and get the data
 try:  
-  dssFile = DSS.open("C:/temp/sample.dss", "10MAR2006 2400, 09APR2006 2400")
+  dssFile = HecDss.open("C:/temp/sample.dss", "10MAR2006 2400, 09APR2006 2400")
   outflow = dssFile.read("/AMERICAN/FOLSOM/FLOW-RES OUT/01JAN2006/1DAY/OBS/")
   newOutflow = outflow.add(10.)
 
@@ -25,6 +25,5 @@ try:
 
   dssFile.write( newOutflow)
 
-except java.lang.Exception, e :
-  #  Take care of any missing data or errors
-   MessageBox.showError(e.getMessage(), "Error reading data")
+except java.lang.Exception, e : #  Take care of any missing data or errors
+  MessageBox.showError(e.getMessage(), "Error reading data")
