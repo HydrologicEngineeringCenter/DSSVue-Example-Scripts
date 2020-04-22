a81251f7d28374a3c21e5d2322c571d8135a8337
Tichansky <1380799754@mil>
2020-04-22 12:54:55 -0700

    Rm display lines, fix imports, add finally, prints

--- src/EstimateMissing.py
+++ src/EstimateMissing.py
@@ -1,16 +1,17 @@
-# name=EstimateMissing
-# displayinmenu=true
-# displaytouser=true
-# displayinselector=true
+
 #  Fill in missing values using HecMath
 #
-from hec.script import *
-from hec.hecmath import *
-from java import *
+from hec.script import Plot, MessageBox
+# from hec.io import TimeSeriesContainer
+# from hec.io import PairedDataContainer
+# from hec.hecmath import TimeSeriesMath
+# from hec.hecmath import PairedDataMath
+from hec.heclib.dss import HecDss, DSSPathname
+import java
 
 #  Open the file and get the data
 try:  
-  dssFile = DSS.open("C:/temp/sample.dss", "10MAR2006 2400, 09APR2006 2400")
+  dssFile = HecDss.open("C:/temp/sample.dss", "10MAR2006 2400, 09APR2006 2400")
   flow = dssFile.read("/AMERICAN/FOLSOM/FLOW-RES IN/01JAN2006/1DAY/OBS/")
 
   newflow = flow.estimateForMissingValues(10)
@@ -21,7 +22,13 @@ try:
   newflow.setPathname(path.getPathname())
 
   dssFile.write( newflow)
+  print "Done"
 
 except java.lang.Exception, e :
   #  Take care of any missing data or errors
    MessageBox.showError(e.getMessage(), "Error reading data")
+
+finally:
+  print "Closing DSS File"
+  dssFile.close()
+    
