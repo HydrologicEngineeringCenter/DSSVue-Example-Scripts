7c12e6bb5a0ed7f77ebc828007eab73ad352b752
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-04-23 18:22:10 -0700

    add vscode task to run jython from hardcoded path

--- runall.bat
+++ runall.bat
@@ -1,7 +1,5 @@
 set J=C:\Programs\CWMS-v3.2.1.256\common\exe\runhere.exe
 cd  %~dp0
 copy data\sample.dss  c:\temp\
-@echo off
-for /r %%v in (src\*.py) do %J% jython src\"%%v" 
-
-pause
+for /r %%f in (src\*.py) do  %J% jython src\%%~nxf 
+  pause
---
+++ scripts/go.bat
@@ -0,0 +1,3 @@
+set J=C:\Programs\CWMS-v3.2.1.256\common\exe\runhere.exe
+call %J% jython %1 > out.txt
+type out.txt
