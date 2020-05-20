24a14873c62cc5dcd5c381c820e52388e2115157
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-05-20 14:35:51 -0700

    redirect output to file, the type to screen

--- scripts/go.bat
+++ scripts/go.bat
@@ -1,14 +1,20 @@
+cd %~dp0
 set J=C:\Programs\CWMS-v3.2.1.256\common\exe\runhere.exe
-call %J% jython %1 %2
+rem run program  
+::call %J% jython %1 %2  
 
+
+rem redirect output to a file, then copy file to screen.
+call %J% jython %1 %2  > tmp{010}.txt 2>&1
+type tmp{010}.txt
 echo done running %1
+
+
 ::start "title" /WAIT /B C:\Programs\CWMS-v3.2.1.256\common\exe\runhere.exe jython VsCode-sample.py .
 
-:: uses existing cscode window  
+:: uses existing vscode window  
 ::start "jython %1" /WAIT /B %J% jython %1 %2 
 
-
 :: new window but closes when done..
-::start "jython %1" /WAIT %J% jython %1 %2
-
+::start "jython %1" /WAIT %J% jython %1 %2  
 
