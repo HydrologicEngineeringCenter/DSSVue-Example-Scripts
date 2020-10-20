e10dd9a1745383583c7dd54cbaa9f7b3a79b1bda
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-10-20 12:09:43 -0700

    updater extended dates example to save to dss

--- src/ExtendedDates.py
+++ src/ExtendedDates.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from hec.heclib.util import HecTime
+from hec.heclib.dss import HecDss, DSSPathname
+from hec.io import TimeSeriesContainer
+from hec.heclib.util import HecTime
+import java
+import sys
+import os
+fileName = "c:/temp/day_granularity.dss"   
+if os.path.isfile(fileName):
+  os.remove(fileName)
+
+dss = HecDss.open(fileName)
+
+tsc = TimeSeriesContainer()
+tsc.fullName = "/test/day_granularity/FLOW/04Sep3000/1YEAR/MODEL/"
 
 
-t = HecTime("21Jan5000",HecTime.DAY_GRANULARITY)
-print t
+tsc.values = range(1,3000,1)
+start = HecTime("04Sep3000", "1330")
+LastYear = 3000
+AnnualTimes = []
+for x in range(len(tsc.values)) :
+ LastYear += 1
+ hecTime = HecTime('31Dec%04d 2400' % LastYear, HecTime.DAY_GRANULARITY)
+ AnnualTimes.append(hecTime.value())
+
+tsc.times = AnnualTimes
+tsc.numberValues = len(tsc.values)
+tsc.units = "CFS"
+tsc.type = "PER-AVER"
+tsc.setTimeGranularitySeconds(86400)
+dss.put(tsc) 
