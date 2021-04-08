d95725c8aab63f763d03fcfa203fb35c2ae7cd88
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2021-04-08 08:45:13 -0700

    simple write to CSV file - forget about Tabulate!

---
+++ src/ExportCSV.py
@@ -0,0 +1,20 @@
+from hec.heclib.dss import HecDss
+import sys
+
+file = R"C:\project\DSSVue-Example-Scripts\data\1PcAEP_360min_5145.dss"
+dssfile = HecDss.open(file)
+
+flow = dssfile.get("//Subbasin-1C2/FLOW//1MIN/RUN:1PcAEP_360min_5145/")
+
+file = open(R'c:\temp\output.csv','w')
+file.write("Subbasin-1C2\n")
+file.write("DateTime,Value\n")
+times = flow.getTimes()
+i =0
+for Q in flow.values:
+  t = times.element(i)
+  #file.write(str(t.year())+"-"+str(t.month())+"-"+str(t.day())+ " "+str(t.hour())+":"+str(t.minute())+"\n")
+  file.write(t.date(-13)+", "+t.time()+"\n")
+  i=i+1
+
+sys.stdin.readline()
