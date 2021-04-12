57f5f193778a088bd5175ad1ce631324debbfc33
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2021-04-12 11:05:24 -0700

    include Q in output

--- src/ExportCSV.py
+++ src/ExportCSV.py
@@ -14,7 +14,7 @@ i =0
 for Q in flow.values:
   t = times.element(i)
   #file.write(str(t.year())+"-"+str(t.month())+"-"+str(t.day())+ " "+str(t.hour())+":"+str(t.minute())+"\n")
-  file.write(t.date(-13)+", "+t.time()+"\n")
+  file.write(t.date(-13)+" "+t.time()+","+str(Q)+"\n")
   i=i+1
 
 sys.stdin.readline()
