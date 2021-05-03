ced99d61c6a83fb5905c2800f0bb5a7dcd7af418
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2021-05-03 14:07:24 -0700

    cleanup

--- src/DataType.py
+++ src/DataType.py
@@ -6,19 +6,14 @@ import sys
 file = R"C:\project\DSSVue-Example-Scripts\data\kinzua.dss"
 path="//AG RESORT/FLOW/01Feb2014/15Minute/FOR:ALTERNATIVE 1:B0C0/"
 
-# tsc =TimeSeriesContainer()
-# tsc.fullName = path
-# dss =HecTimeSeries(file)
-# status = dss.read(tsc, True)
-# print("precision:",tsc.precision)
-# print(dss.getRecordInfo(path).recordType)
-# print (tsc.type)
-# -------------------
 dss2 = HecDss.open(file)
 flow = dss2.get(path)
 print (flow.numberValues )
 print("flow.dataType =",flow.dataType)
 
+# -- Output ---
+# 529
+# ('flow.dataType =', 105)
 
 
 
