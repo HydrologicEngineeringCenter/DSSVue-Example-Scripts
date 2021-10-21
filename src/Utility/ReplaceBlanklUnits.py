7e5811dd31954b9dbb562ba4549aa1123cb6ea0e
Karl Tarbet <ktarbet@users.noreply.github.com>
2021-10-21 07:35:23 -0700

    Update ReplaceBlanklUnits.py

--- src/Utility/ReplaceBlanklUnits.py
+++ src/Utility/ReplaceBlanklUnits.py
@@ -1,18 +1,17 @@
-#replace blank/empty units with  'kcfs'
 from hec.heclib.dss import HecDss
 from hec.io import TimeSeriesContainer,PairedDataContainer
 from hec.io import DataContainer
 
-dss = HecDss.open("C:/temp/ensemble_test1.dss")
+dss = HecDss.open("C:/temp/a.dss")
 paths = dss.getPathnameList()
 for p in paths:
     o = dss.get(p)
-    if isinstance(o ,TimeSeriesContainer) 
+    if isinstance(o ,TimeSeriesContainer): 
         print("units: '" + o.units+"'")
         if o.units is None or o.units.strip() == '':
             o.units = 'kcfs'
             dss.put(o)
-    if isinstance(o ,PairedDataContainer) 
+    if isinstance(o ,PairedDataContainer): 
         print("units: '" + o.yunits+"'")
         if o.units is None or o.xunits.strip() == '':
             o.xunits = 'x'
