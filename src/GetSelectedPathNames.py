4cec83b3b52cde0b140242f7132dc6f371c68880
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2023-09-01 15:05:50 -0700

    add more to the GetSelectedPathNames example

--- src/GetSelectedPathNames.py
+++ src/GetSelectedPathNames.py
@@ -1,10 +1,20 @@
 from hec.dssgui import ListSelection
+from hec.heclib.dss import DSSPathname
 
 listSelection = ListSelection.getMainWindow()
 
 count = listSelection.getNumberSelectedPathnames()
 print(count)
-
 vector = listSelection.getSelectedPaths()
 for p in vector:
  print(p)
+ print(type(p))
+
+
+ ds = listSelection.getSelectionList()     # DataReferenceSet
+ dr = ds.get(0) # get first selection (DataReference)
+print(dr.pathname())
+dssPath = DSSPathname(dr.pathname())  
+
+title = dssPath.bPart()
+print(title)
