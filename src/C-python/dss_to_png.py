f15d4293552b773724eadd71e21d06706ea9d722
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-09-23 09:32:39 -0700

    update example that writes png, and used plotly

--- src/C-python/dss_to_png.py
+++ src/C-python/dss_to_png.py
@@ -1,64 +1,52 @@
-#from datetime import datetime
-from pydsstools.heclib.dss import HecDss
 import numpy as np
+from hecdss import HecDss
+from datetime import datetime
 
-def tscSplitter(path):
-    'function to read data from dss and return list of times and values'
-    global dssFile, startDay,startTime,endDay,endTime
-    tsc = dssFile.read_window(path,startDay,startTime,endDay,endTime)
-    times = tsc.pytimes
-    values = list(tsc.values)
-    return times, values
+t1 = datetime(2020, 8, 1)
+t2 = datetime(2020, 8, 31)
 
-#time window
-startDay = "01AUG2020"
-startTime ="24:00"
-endDay = "31AUG2020"
-endTime = "24:00"
-#dss path
-dss_file_path = r'sampleDssFile.dss'
-#ts path
-tsElevPath = '/SHEYENNE RIVER/BALDHILL DAM/ELEV//1Hour/COMP/'
-#open dss file
-dssFile =  HecDss.Open(dss_file_path)
-#get times and values with tscSplitter function
-timeValue, elevValue = tscSplitter(tsElevPath)
-#ts path
-tsFlowPath = '/SHEYENNE RIVER/BALDHILL DAM/FLOW-OUT//1Hour/COMP/'
-#get times and values with tscSplitter function
-timeValue, flowValue = tscSplitter(tsFlowPath)
-# close dss file
-dssFile.close()
+try:
+    dss_file_path = r'sampleDssFile.dss'
+    dss = HecDss(dss_file_path)
 
-##########plot it
+    tsElevPath = '/SHEYENNE RIVER/BALDHILL DAM/ELEV//1Hour/COMP/'
+    tsc_elev = dss.get(tsElevPath)
+
+    tsFlowPath = '/SHEYENNE RIVER/BALDHILL DAM/FLOW-OUT//1Hour/COMP/'
+    tsc_flow = dss.get(tsFlowPath)
+finally:
+    dss.close()
+
+########## plot it
 
 import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
+
 pio.renderers
 pio.renderers.default = "browser"
 
-#make figure w/ 2 viewports
-fig  = make_subplots(rows=2, cols=1, shared_xaxes=True)
+# make figure w/ 2 viewports
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
 
 #########Top Viewport
 # add elevations
 fig.add_trace(go.Scatter(
-    x=timeValue, y = elevValue, name='Elevation'))
+    x=tsc_elev.times, y=tsc_elev.values, name='Elevation'))
 
 # add flows
 fig.add_trace(go.Scatter(
-    x=timeValue, y=flowValue,
+    x=tsc_flow.times, y=tsc_flow.values,
     name='Outflow', showlegend=True),
-    row=2, col=1, 
-             )
+    row=2, col=1,
+)
 # set title
-fig.update_layout( title='Reservoir Plot', showlegend=True)
+fig.update_layout(title='Reservoir Plot', showlegend=True)
 # Set y-axes titles
 fig.layout.yaxis1.update({'title': "<b>Elevation, ft NGVD29</b>"})
 fig.layout.yaxis2.update({'title': "<b>Discharge, ft<sup>3</sup>/s</b>"})
 
 # save plot
 fig.write_image("sample.png")
-#type fig in console to display interactive plot
+# type fig in console to display interactive plot
 fig.write_html("c:/temp/file.html")
