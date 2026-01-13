e97911ed20d7a1cbb3a3b8dedfae92c6af7c529e
Karl Tarbet <ktarbet@users.noreply.github.com>
2026-01-13 08:10:00 -0800

    Add conversion function example for DSS version 7
    Implement conversion of DSS files to version 7 format.

---
+++ src/convert_to_dss_version_7.py
@@ -0,0 +1,39 @@
+converfrom hec.heclib.dss import HecDss
+from hec.heclib.dss import HecDSSUtilities
+from hec.heclib.util import Heclib
+import os
+
+
+def delete_file(filename):
+  if os.path.exists(filename):
+    try:
+        os.remove(filename)
+    except OSError as e:
+        print "Error: deleting  file "+filename
+
+def convert_to_dss_version_7(filename):
+  """ 
+  converts filename to a version 7 DSS file if necessary.
+  """
+  if Heclib.zgetFileVersion(filename) == 7:
+    print("already  a version 7 file")
+    return
+  base_name, extension = os.path.splitext(filename)
+  tmp_filename = base_name + "_backup"+  extension
+  delete_file(tmp_filename)
+  os.rename(filename, tmp_filename)
+  h = HecDSSUtilities()
+  h.setDSSFileName(tmp_filename)
+  h.convertVersion(filename)
+  h.close()
+
+
+filename= r"c:\tmp\forecast.dss"
+delete_file(filename)
+dss6 = HecDss.open(filename,6)  # force a dss 6 file for testing only works with older software.
+dss6.close()
+
+convert_to_dss_version_7(filename)
+convert_to_dss_version_7(filename)  # try again, but already version 7 
+
+print("done")
